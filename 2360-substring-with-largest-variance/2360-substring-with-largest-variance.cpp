ea73dab5756a297c41e22a1ef0fae61936dfbc26
Ash-So <142682875+Ash-So@users.noreply.github.com>
2023-10-20 12:33:40 +0530

    Time: 358 ms (9.77%), Space: 7.5 MB (23.65%) - LeetHub

---
+++ 2360-substring-with-largest-variance/2360-substring-with-largest-variance.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int largestVariance(string s) {
+        int ans=0;
+        unordered_map<char, int> freq;
+
+        for(auto& c:s){
+            freq[c]++;
+        }
+
+        for(char ch1='a';ch1<='z';ch1++){
+            for(char ch2='a';ch2<='z';ch2++){
+                if( ch1 == ch2 || freq[ch1] == 0 || freq[ch2] == 0 ) continue;
+
+                for(int rev=1;rev<=2;rev++){
+                    int c1=0, c2=0;
+                    for(auto& c:s){
+                        c1+= c==ch1;
+                        c2+= c==ch2;
+
+                        if(c1<c2) c1=c2=0;
+                        if(c1>0 & c2>0) ans=max(ans,c1-c2);
+                    }
+                    reverse(s.begin(), s.end());
+                }
+            }   
+        }
+        return ans;
+    }
+};
