83095a078a43e91d22bfbe81dd963137e0217b95
Ash-So <142682875+Ash-So@users.noreply.github.com>
2023-10-25 09:35:35 +0530

    Time: 40 ms (29.32%), Space: 14.6 MB (7.97%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
@@ -5,7 +5,7 @@ public:
         int n = str1.size();
         int m = str2.size();
         int j=0;
-        for(int i=0; i<n && j<n ;i++){
+        for(int i=0; i<n && j<m ;i++){
             if(str1[i] == str2 [j] || str1[i]+1 == str2[j] || str1[i] - 25 == str2[j]){
                 j++;
             }
